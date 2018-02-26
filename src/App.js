eb98e13f702c6667a8b2cbaa8c311fb19ca8f9d3
NickGatti <nick.gatti@gmail.com>
2018-02-26 14:06:17 -0700

    update

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import './App.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import ShoppingCart from './components/ShoppingCart'
-
-let applicationState = {
-  items: [
-    { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
-    { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
-    { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
-  ]
-}
+import AddItem from './components/AddItem'
 
 class App extends Component {
+  state = {
+    items: [
+      { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
+      { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
+      { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <Header />
-        <ShoppingCart items={applicationState.items}/>
+        <ShoppingCart items={this.state.items}/>
+        <AddItem />
         <Footer year="2018"/>
       </div>
     );
